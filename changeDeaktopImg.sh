8ada399982bde580e20725ea1274c315c8dbf4d3
tinet-shenjg <jgshen@foxmail.com>
2019-10-24 18:03:57 +0800

    一键替换mac桌面背景

--- changeDeaktopImg.sh
+++ changeDeaktopImg.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+python3 desk.py
+sleep 1  #等1秒后执行下一条
 localpath="/Users/$USER/dev/tool/desk/background.jpg"
+echo $localpath
 osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$localpath\""
 
